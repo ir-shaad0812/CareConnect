8018feadd8ace7e2eac52217ab2aab6dfe4d1df4
ir-shadd0812 <irshad.aalam0812@gmail.com>
2025-11-27 10:01:00 +0545

    feat(backend): add Backend/src/routes/index.js

---
+++ Backend/src/routes/index.js
@@ -0,0 +1,59 @@
+import authRoutes from "./auth.routes.js";
+import agreementRoutes from "../modules/agreement/agreement.routes.js";
+import userRoutes from "./user.routes.js";
+import documentRoutes from "./document.routes.js";
+import adminRoutes from "./admin.routes.js";
+import bookingRoutes from "./booking.routes.js";
+import notificationRoutes from "./notification.routes.js";
+import chatRoutes from "./chat.routes.js";
+import searchRoutes from "./search.routes.js";
+import trackRoutes from "./track.routes.js";
+import trackingRoutes from "./tracking.routes.js";
+import videoRoutes from "./video.routes.js";
+import paymentRoutes from "./payment.routes.js";
+import reviewRoutes from "./review.routes.js";
+import aiMatchRoutes from "./aiMatch.routes.js";
+import jobRoutes from "./job.routes.js";
+import adminJobRoutes from "./adminJob.routes.js";
+import supportRoutes from "./support.routes.js";
+import locationRoutes from "./location.routes.js";
+import availabilityRoutes from "./availability.routes.js";
+import noteRoutes from "./note.routes.js";
+import walletRoutes from "./wallet.routes.js";
+import dashboardRoutes from "./dashboard.routes.js";
+import disputeRoutes from "./dispute.routes.js";
+import noticeRoutes from "./notice.routes.js";
+import taskRoutes from "./task.routes.js";
+import aiRoutes from "./ai.routes.js";
+import feedbackRoutes from "./feedback.routes.js";
+
+export {
+  authRoutes,
+  agreementRoutes,
+  userRoutes,
+  documentRoutes,
+  adminRoutes,
+  bookingRoutes,
+  notificationRoutes,
+  chatRoutes,
+  searchRoutes,
+  trackRoutes,
+  trackingRoutes,
+  videoRoutes,
+  paymentRoutes,
+  reviewRoutes,
+  aiMatchRoutes,
+  jobRoutes,
+  adminJobRoutes,
+  supportRoutes,
+  locationRoutes,
+  availabilityRoutes,
+  noteRoutes,
+  walletRoutes,
+  dashboardRoutes,
+  disputeRoutes,
+  noticeRoutes,
+  taskRoutes,
+  aiRoutes,
+  feedbackRoutes,
+};
