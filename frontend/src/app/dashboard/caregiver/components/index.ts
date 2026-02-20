3f80b1ed0be93928dc3d5a9b4101df000efd4b21
ir-shadd0812 <irshad.aalam0812@gmail.com>
2026-02-20 10:02:00 +0545

    feat(frontend): add frontend/src/app/dashboard/caregiver/components/index.ts

---
+++ frontend/src/app/dashboard/caregiver/components/index.ts
@@ -0,0 +1,22 @@
+export { default as CaregiverLayout } from "./CaregiverLayout";
+export { default as CaregiverSidebar } from "./CaregiverSidebar";
+export { default as CaregiverTopBar } from "./CaregiverTopBar";
+export { default as StatsCards } from "./StatsCards";
+export { default as DashboardCharts } from "./DashboardCharts";
+export { default as JobListings } from "./JobListings";
+export { default as MessagingPanel } from "./MessagingPanel";
+export { default as RecentActivity } from "./RecentActivity";
+export { default as WelcomeBanner } from "./WelcomeBanner";
+export {
+  ProfileCard,
+  VerificationWidget,
+  UpcomingSchedule,
+  TodayTasksWidget,
+  PendingActionsWidget,
+  RatingBreakdown,
+  QuickActions,
+  NoticesWidget,
+  NotesWidget,
+} from "./ProfileWidgets";
+export { default as BookingStatusWidget } from "./BookingStatusWidget";
+export { default as RealTimeIndicator } from "./RealTimeIndicator";
