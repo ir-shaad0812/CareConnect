40949dc69688922c45c3f0ac8a4be2b692026bfb
ir-shadd0812 <irshad.aalam0812@gmail.com>
2026-03-25 10:00:00 +0545

    feat(backend): add Backend/src/modules/booking/index.js

---
+++ Backend/src/modules/booking/index.js
@@ -0,0 +1,4 @@
+export * from './booking.controller.js';
+export { default as bookingService } from './booking.service.js';
+export { default as bookingRoutes } from './booking.routes.js';
+export * from './booking.validation.js';
