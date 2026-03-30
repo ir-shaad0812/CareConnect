22d8f8cea423c02be1bee6fe020f15f8591c72e8
ir-shadd0812 <irshad.aalam0812@gmail.com>
2026-03-30 10:00:00 +0545

    feat(backend): add Backend/src/modules/chat/index.js

---
+++ Backend/src/modules/chat/index.js
@@ -0,0 +1,4 @@
+export { default as chatController } from './chat.controller.js';
+export { default as chatService } from './chat.service.js';
+export { default as chatRoutes } from './chat.routes.js';
+export * from './chat.validation.js';
