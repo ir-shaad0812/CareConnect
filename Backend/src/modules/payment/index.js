6e68a4dcea83bc981996cfcf3ddd376641166399
ir-shadd0812 <irshad.aalam0812@gmail.com>
2026-04-01 10:00:00 +0545

    feat(backend): add Backend/src/modules/payment/index.js

---
+++ Backend/src/modules/payment/index.js
@@ -0,0 +1,4 @@
+export * from './payment.controller.js';
+export { default as paymentService } from './payment.service.js';
+export { default as paymentRoutes } from './payment.routes.js';
+export * from './payment.validation.js';
