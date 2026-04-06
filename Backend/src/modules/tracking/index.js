e7e61b3131a27fc5c45e4ff03007bfb7a68c1872
ir-shadd0812 <irshad.aalam0812@gmail.com>
2026-04-06 10:00:00 +0545

    feat(backend): add Backend/src/modules/tracking/index.js

---
+++ Backend/src/modules/tracking/index.js
@@ -0,0 +1,4 @@
+export * from './tracking.controller.js';
+export * from './tracking.service.js';
+export { default as trackingRoutes } from './tracking.routes.js';
+export * from './tracking.validation.js';
