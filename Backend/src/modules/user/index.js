4116423f4143a2a0c2ac1a2a43be8045e18d3a10
ir-shadd0812 <irshad.aalam0812@gmail.com>
2026-04-11 10:00:00 +0545

    feat(backend): add Backend/src/modules/user/index.js

---
+++ Backend/src/modules/user/index.js
@@ -0,0 +1,4 @@
+export * from './user.controller.js';
+export { default as userService } from './user.service.js';
+export { default as userRoutes } from './user.routes.js';
+export * from './user.validation.js';
